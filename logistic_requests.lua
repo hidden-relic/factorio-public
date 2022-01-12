2fbfebc2774a49bb42411db5f3a65362bf53c17c
hidden-relic <78228823+hidden-relic@users.noreply.github.com>
2022-01-11 21:48:56 -0500

    Update logistic_requests.lua

--- logistic_requests.lua
+++ logistic_requests.lua
@@ -5,20 +5,14 @@ local function stack_size(item)
 end
 
 local list = {
-    name = {
         "electric-mining-drill", "stone-wall", "gun-turret", "radar",
         "fast-transport-belt", "fast-splitter", "fast-underground-belt",
         "fast-inserter", "medium-electric-pole", "assembling-machine-2",
         "express-transport-belt", "express-splitter",
         "express-underground-belt", "stack-inserter", "substation",
         "assembling-machine-3", "beacon", "big-electric-pole"
-    },
-    min = {},
-    max = {}
 }
-for i, item in pairs(list.name) do
-    list.min[i],  list.max[i] = stack_size(item)
-end
-for i, item in pairs(list) do
+for i = 1, #list do
+    item = {name=list[i], min=stack_size(list[i]), max=stack_size(list[i])*2}
     game.player.set_personal_logistic_slot(i, item)
 end
