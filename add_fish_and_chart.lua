87b4b70b5fccab308f7c9a6ef4a9560605c5285c
hidden-relic <78228823+hidden-relic@users.noreply.github.com>
2022-03-29 06:38:37 -0400

    Update add_fish_and_chart.lua

--- add_fish_and_chart.lua
+++ add_fish_and_chart.lua
@@ -3,7 +3,7 @@ local total = 0
 local search = {"water", "deepwater", "water-green", "deepwater-green"}
 for _, player in pairs(game.connected_players) do
     for _, watertype in pairs(search) do
-        local results = game.player.surface.find_tiles_filtered({name=watertype, position=global.ocore.playerSpawns[player.name], radius=r})
+        local results = game.player.surface.find_tiles_filtered({name=watertype, position=player.position, radius=r})
         for i = 1, #results, 16 do
             player.force.chart(player.surface.name, {{results[i].position.x, results[i].position.y}, {results[i].position.x+1, results[i].position.y+1}})
             player.surface.create_entity{name="fish", position=results[i].position}
