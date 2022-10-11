eed6617e5e10c0a93a34e8f89667c1d5a463bf4a
hidden-relic <78228823+hidden-relic@users.noreply.github.com>
2022-10-11 07:06:45 -0500

    Update useful_commands.lua

--- useful_commands.lua
+++ useful_commands.lua
@@ -3,3 +3,33 @@ local r=10 for _, bug in pairs(game.player.surface.find_entities_filtered{type="
 
 -- chart all
 game.player.force.chart_all()
+
+-- get total ingredients for 1 of every item
+local t = {}
+
+for name, item in pairs(game.item_prototypes) do
+    
+    if not t[name] then
+        t[name] = 1
+    end
+    
+    pcall(function ()
+        for _, ingredient in pairs(game.recipe_prototypes[name].ingredients) do        
+            if not t[ingredient.name] then         
+                if ingredient.amount then
+                    t[ingredient.name] = ingredient.amount
+                else
+                    t[ingredient.name] = 1
+                end    
+            else        
+                if ingredient.amount then
+                    t[ingredient.name] = t[ingredient.name] + ingredient.amount
+                else
+                    t[ingredient.name] = t[ingredient.name] + 1
+                end
+            end
+        end
+    end)
+end
+
+game.write_file("ingredients.lua", serpent.block(t))
